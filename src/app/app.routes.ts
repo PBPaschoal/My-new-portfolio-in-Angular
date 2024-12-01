cd700a73e24ead1dda486f1d53ba169b6f623c7d
Paulo Paschoal <programming.paschoal@gmail.com>
2024-12-01 18:55:05 -0300

    Ajustando algumas coisas, criando outras e colocando o site online

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,24 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
-import { ContatosComponent } from './components/contatos/contatos.component';
 import { PrimeiraSectionComponent } from './components/primeira-section/primeira-section.component';
-import { UxComponent } from './components/ux/ux.component'; // Novo componente
-import { CursosComponent } from './components/cursos/cursos.component'; 
-import { BackendComponent } from './components/backend/backend.component'; // Novo componente
-import { FrontendComponent } from './components/frontend/frontend.component'; // Novo componente
-import { ExperienciasComponent } from './components/experiencias/experiencias.component'; // Novo componente
-import { AdmComponent } from './components/adm/adm.component'; // Novo componente
-import { TiComponent } from './components/ti/ti.component'; // Novo componente
+import { ContatosComponent } from './components/contatos/contatos.component';
+import { UxComponent } from './components/ux/ux.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { BackendComponent } from './components/backend/backend.component';
+import { FrontendComponent } from './components/frontend/frontend.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { AdmComponent } from './components/adm/adm.component';
+import { TiComponent } from './components/ti/ti.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 export const routes: Routes = [
-  { path: '', component: PrimeiraSectionComponent }, // Página inicial
-  { path: 'contatos', component: ContatosComponent }, // Página de contatos
-  { path: 'ux', component: UxComponent }, // Nova rota para o componente UX
-  { path: 'cursos', component: CursosComponent }, // Cursos
-  { path: 'backend', component: BackendComponent }, // Nova rota para o componente Backend
-  { path: 'frontend', component: FrontendComponent }, // Nova rota para o componente Frontend
-  { path: 'experiencias', component: ExperienciasComponent }, // Nova rota para o componente Experiências
-  { path: 'adm', component: AdmComponent }, // Nova rota para o componente Adm
-  { path: 'ti', component: TiComponent }, // Nova rota para o componente TI
+  { path: '', component: PrimeiraSectionComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: 'ux', component: UxComponent },
+  { path: 'cursos', component: CursosComponent },
+  { path: 'backend', component: BackendComponent },
+  { path: 'frontend', component: FrontendComponent },
+  { path: 'experiencias', component: ExperienciasComponent },
+  { path: 'adm', component: AdmComponent },
+  { path: 'ti', component: TiComponent },
+  { path: 'sobre', component: SobreComponent },
 ];
